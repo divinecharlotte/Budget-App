9a2259a1129368a22b6d85c679e25d9315956a01
charlotte divine <divinemaina@gmail.com>
2022-10-11 11:53:59 -0700

    add references to tables and rename some columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_181624) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_11_185210) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.string "icon"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
-    t.index ["author_id"], name: "index_categories_on_author_id"
-  end
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "transactions", force: :cascade do |t|
-    t.string "transaction_name"
-    t.integer "amount"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
-    t.bigint "category_id", null: false
-    t.index ["author_id"], name: "index_transactions_on_author_id"
-    t.index ["category_id"], name: "index_transactions_on_category_id"
+    t.index ["author_id"], name: "index_groups_on_author_id"
   end
 
   create_table "user_transactions", force: :cascade do |t|
@@ -46,22 +28,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_181624) do
     t.integer "amount", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.bigint "group_id", null: false
+    t.index ["author_id"], name: "index_user_transactions_on_author_id"
+    t.index ["group_id"], name: "index_user_transactions_on_group_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "categories", "users", column: "author_id"
-  add_foreign_key "transactions", "categories"
-  add_foreign_key "transactions", "users", column: "author_id"
+  add_foreign_key "groups", "users", column: "author_id"
+  add_foreign_key "user_transactions", "groups"
+  add_foreign_key "user_transactions", "users", column: "author_id"
 end
