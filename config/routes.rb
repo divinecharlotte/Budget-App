01f27a7d1af40918629e077a148e289d829a751a
charlotte divine <divinemaina@gmail.com>
2022-10-12 21:49:38 -0700

    add logout feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_scope :user do  
-    get '/users/sign_out' => 'devise/sessions#destroy'     
-  end
 
   resources :groups do
     resources :user_transactions
   end
-  root "home#index"
-  authenticated :user do
-    root :to => 'groups#index', as: :authenticated_root
-  end
+  root "groups#index"
 
 end
